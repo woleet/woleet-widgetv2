d4cb9d56c0009dcb1613601c1077949a157a4ba0
Aleksey Mordvinov <Aleksey.Mordvinov@noveogroup.com>
2019-03-29 12:42:53 +0700

    remove dist from the repo because it's too heavy

--- dist/pdf.js.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{91:function(n,o){},92:function(n,o){},93:function(n,o){},94:function(n,o){}}]);
-//# sourceMappingURL=pdf.js.js.map
--- dist/woleet-weblibs.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{114:function(n,o){},116:function(n,o){},137:function(n,o){},141:function(n,o){}}]);
-//# sourceMappingURL=woleet-weblibs.js.map
