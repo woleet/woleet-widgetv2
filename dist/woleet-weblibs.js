ed82c4a65dea971fee553a59ba3e56a2bdb09c15
Aleksey Mordvinov <Aleksey.Mordvinov@noveogroup.com>
2019-04-26 16:49:29 +0700

    https://woleet.atlassian.net/browse/WV2-36 - in progress

--- dist/woleet-weblibs.js
+++ dist/woleet-weblibs.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{100:function(n,o){},104:function(n,o){},77:function(n,o){},79:function(n,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{100:function(n,o){},104:function(n,o){},77:function(n,o){},79:function(n,o){}}]);
 //# sourceMappingURL=woleet-weblibs.js.map
