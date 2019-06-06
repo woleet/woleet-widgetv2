a774151e307e9600422804b2cffc78c86e007f58
Quang Le <quang.le@woleet.com>
2019-06-06 14:11:34 +0200

    Revert the 'webpack.dev.config.js'

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -42,7 +42,7 @@ config.devServer = {
  */
 config.plugins.push(new HtmlWebpackPlugin({
   filename: resourcePath + 'index.html',
-  template: 'examples/file-hasher-widget-display-tests.html',
+  template: 'examples/dev-' + widgetLabel + '.html',
   inject: false
 }));
 
