a335f473257cd8df94bad7a4ee34a265b6a051b3
Quang Le <quang.le@woleet.com>
2019-11-04 09:31:25 +0100

    Add others languages

--- dist/pdf.js.js
+++ dist/pdf.js.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{117:function(n,o){},118:function(n,o){},119:function(n,o){},120:function(n,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{119:function(n,o){},120:function(n,o){},121:function(n,o){},122:function(n,o){}}]);
 //# sourceMappingURL=pdf.js.js.map
--- dist/woleet-weblibs.js
+++ dist/woleet-weblibs.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{66:function(n,o){},68:function(n,o){},91:function(n,o){},95:function(n,o){}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{70:function(n,o){},72:function(n,o){},96:function(n,o){},99:function(n,o){}}]);
 //# sourceMappingURL=woleet-weblibs.js.map
