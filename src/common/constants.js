a7c9613c47fdac1bd8fa7c8ec6c3a053dfa0fd0f
vbarat <vincent.barat@woleet.com>
2020-09-09 14:04:14 +0200

    Comment

--- src/common/constants.js
+++ src/common/constants.js
@@ -6,6 +6,6 @@ const FILE_HASHER_WIDGET_ID = 'file-hasher-widget';
 const FILE_HASHER_WIDGET_TYPE = 'FILE_HASHER_WIDGET';
 
 export default {
-  FILE_HASHER_WIDGET_ID, /* export file-hasher-widget constants */
+  FILE_HASHER_WIDGET_ID,
   FILE_HASHER_WIDGET_TYPE
 };
